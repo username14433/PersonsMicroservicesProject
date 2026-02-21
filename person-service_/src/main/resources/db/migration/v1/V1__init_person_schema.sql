c212cf8933292d9dbbaa5cd490b806d3eee6f5a1
username14433 <burilinvladimir150507@gmail.com>
2026-02-22 01:47:26 +0300

    feat: Updated application.yml file
    - Described the configuration for connecting to the database
    - Described the flyway configuration for correct execution of database migrations
    - Described the configuration for JPA, namely for hibernate and hibernate envers for auditing changes in the database
    - Described the otel configuration
    - Custom logging format set using logback
    
    feat: logback.xml file created
    
    It describes the logging format in the application
    
    feat: SQL files created for database migrations
    
    - V0__init_schema.sql - file for creating all SQL schemas
    - V1__init_person_schema.sql - file for creating tables in the person schema:  countries, addresses, users, individuals with a detailed description of their columns
    - V2__fill_countries.sql - file for filling the countries table with data
    - V3__init_person_history_schema.sql - file for creating tables in the person_history schema: revinfo, countries_history, addresses_history, users_history, individuals_history with a detailed description of their columns, indexes, and constraints

---
+++ person-service_/src/main/resources/db/migration/v1/V1__init_person_schema.sql
@@ -0,0 +1,45 @@
+CREATE TABLE person.countries
+(
+    id      SERIAL PRIMARY KEY,
+    active  BOOLEAN                     NOT NULL DEFAULT TRUE,
+    created TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+    updated TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+    name    VARCHAR(128)                NOT NULL,
+    code    VARCHAR(3)
+);
+
+CREATE TABLE person.addresses
+(
+    id         uuid PRIMARY KEY                     DEFAULT uuid_generate_v4(),
+    active     BOOLEAN                     NOT NULL DEFAULT TRUE,
+    created    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+    updated    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+    country_id INTEGER                     NOT NULL REFERENCES person.countries (id),
+    address    VARCHAR(128)                NOT NULL,
+    zip_code   VARCHAR(32)                 NOT NULL,
+    city       VARCHAR(64)                 NOT NULL
+
+);
+
+CREATE TABLE person.users
+(
+    id         uuid PRIMARY KEY                     DEFAULT uuid_generate_v4(),
+    active     BOOLEAN                     NOT NULL DEFAULT TRUE,
+    created    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+    updated    TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+    email      VARCHAR(1024)               NOT NULL,
+    first_name VARCHAR(64)                 NOT NULL,
+    last_name  VARCHAR(64)                 NOT NULL,
+    address_id UUID                        NOT NULL REFERENCES person.addresses (id)
+);
+
+CREATE TABLE person.individuals
+(
+    id              UUID PRIMARY KEY                     DEFAULT uuid_generate_v4(),
+    active          BOOLEAN                     NOT NULL DEFAULT TRUE,
+    created         TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+    updated         TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (now() AT TIME ZONE 'utc'),
+    passport_number VARCHAR(64)                 NOT NULL,
+    phone_number    VARCHAR(64)                 NOT NULL,
+    user_id         UUID                        NOT NULL REFERENCES person.users (id)
+);
